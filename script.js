e5291a5accb443d2e81aaea17fb41058a64b3e71
mustafAI-GH <koc.mustafaa1@gmail.com>
2026-05-11 14:16:27 +0200

    DOCS: Exercices Variables - JS

---
+++ script.js
@@ -0,0 +1,32 @@
+const tableau = {
+    nom: 'Koc',
+    prenom: 'Mustafa',
+    age: 29 
+}
+console.log(tableau)
+
+let name = 'Mustafa';
+console.log(name)
+
+let num = [5, 6 ,5 ,6 ,5];
+console.log(num)
+
+let color = document.querySelector("h1")
+color.style.fontFamily = "Arial";
+color.style.backgroundColor = "red";
+color.style.fontSize = "3em";
+color.style.textAlign = "center";
+color.style.padding = "30px";
+color.style.margin = "50px";
+color.style.border = "10px solid black";
+color.style.border = " 20px dashed chartreuse";
+
+let paragraphe = document.querySelector("p");
+
+paragraphe.style.textAlign = "center";
+paragraphe.style.fontSize = "2em";
+paragraphe.style.background = "lightgray";
+paragraphe.style.border = "5px dashed black";
+paragraphe.style.borderTop = " 6px ridge purple"; 
+
+
